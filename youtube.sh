3edaf79aff5d5a6ada2638adcf18cce1a721af29
etnarek <romain-fontaine@outlook.com>
2013-10-28 15:01:32 +0100

    corrected some stuff

--- youtube.sh
+++ youtube.sh
@@ -3,4 +3,4 @@ umask 000
 youtube-dl -x $1 --audio-format mp3 --restrict-filenames -w -c -o "/home/youtube/mp3/%(title)s.%(ext)s"
 NEW=$(youtube-dl --get-filename $1 --restrict-filenames -w -c -o "%(title)s.mp3")
 mpc update youtube
-mpc add youtube/mp3/$NEW
+mpc add youtube/mp3/"$NEW"

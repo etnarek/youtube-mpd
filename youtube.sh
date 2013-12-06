f4c39d810d8f2aa9afdf9d79c5d9c53189253b30
iTitou <titouanchristophe@gmail.com>
2013-12-06 01:41:01 +0100

    [fix] Wait for update completion

--- youtube.sh
+++ youtube.sh
@@ -2,5 +2,5 @@
 umask 000
 youtube-dl -x $1 --audio-format mp3 --restrict-filenames -w -c -o "/home/youtube/mp3/%(title)s.%(ext)s"
 NEW=$(youtube-dl --get-filename $1 --restrict-filenames -w -c -o "%(title)s.mp3")
-mpc update youtube
+mpc update --wait youtube
 mpc add youtube/mp3/"$NEW"

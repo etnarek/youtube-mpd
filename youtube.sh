a4e6b2c7415b8597f069e1427ee5b292e265ff84
iTitou <titouanchristophe@gmail.com>
2013-12-16 20:06:54 +0100

    [fix] Add multiple sounds from playlists

--- youtube.sh
+++ youtube.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+#Youtube's mpd relative path
+MPD_YOUTUBE="youtube/mp3"
+
+#Absolute path to the youtube library (physical location)
+DESTINATION="/home/$MPD_YOUTUBE"
+
 umask 000
-youtube-dl -x $1 --audio-format mp3 --restrict-filenames -w -c -o "/home/youtube/mp3/%(title)s.%(ext)s"
-NEW=$(youtube-dl --get-filename $1 --restrict-filenames -w -c -o "%(title)s.mp3")
-mpc update --wait youtube
-mpc add youtube/mp3/"$NEW"
+youtube-dl -x $1 --audio-format mp3 --restrict-filenames -w -c -o "$DESTINATION/%(title)s.%(ext)s"
+for downloaded in $(youtube-dl --get-filename $1 --restrict-filenames -w -c -o "%(title)s.mp3"); do
+  mpc update --wait $MPD_YOUTUBE/$downloaded
+  mpc add $MPD_YOUTUBE/$downloaded
+done
